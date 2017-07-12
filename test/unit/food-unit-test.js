6db78640b604ef1ad137fd6f907d9a18b029fa34
lukyans <sukyans@gmail.com>
2017-07-11 19:19:09 -0600

    add food test

--- test/unit/food-unit-test.js
+++ test/unit/food-unit-test.js
@@ -1,40 +1,27 @@
-// const assert = require('chai').assert
-// const Meal = require('../../lib/meals')
-// const Food = require('../../lib/foods')
-//
-// describe('Food', function() {
-//   it('can turn an API response into HTML', function() {
-//     var APIresponse = new Promise(function(resolve){
-//       resolve({
-//         name: 'buritto',
-//         calories: 150
-//       })
-//       if(false) reject("nope")
-//     })
-//     //Food.
-//   })
-// })
-//
-// // describe('Meal', function() {
-// //   it('can turn an API response into HTML', function() {
-// //     var APIresponse = new Promise(function(resolve){
-// //     resolve({
-// //       name: "Breakfast",
-// //       caloric_goal: 200,
-// //       foods: [
-// //         {
-// //           name: "Banana",
-// //           calories: 35
-// //         }
-// //       ]
-// //     })
-// //     if(false) reject("nope")
-// //   })
-// //     Meal.fromAPIToHTML(APIresponse)
-// //     .then(fuction(mealHTML){
-// //       assert.include(mealHTML, "Banana")
-// //         done()
-// //     }
-// //
-// //   })
-// // })
+const assert = require('chai').assert
+const Meal = require('../../lib/meals')
+const Food = require('../../lib/foods')
+const pry = require("pryjs")
+
+describe('Food', function() {
+  it('can turn an API response into HTML', function(done) {
+    var APIResponse = new Promise (function(resolve){
+      resolve([{
+        name: 'burrito',
+        calories: 150
+      },
+      {
+      name: 'salt',
+      calories: 0
+      }])
+      if(false) reject("nope")
+    })
+    Food.buildFoodsTable(APIResponse)
+    .then(function(foodHTML){
+      assert(foodHTML.some(function(htmlRow){
+        return htmlRow.includes("burrito")
+      }))
+      done()
+    })
+  })
+})
